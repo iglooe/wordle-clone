98e14bc76178c03e179e06ef1e11ca7cf5a992b0
iglooe <day.derek7@protonmail.com>
2023-09-22 00:58:35 -0500

    animation after guess submission

--- app/components/Row.js
+++ app/components/Row.js
@@ -1,21 +1,47 @@
 import React from "react";
 
-export default function Row({ guess }) {
+export default function Row({ guess, currentGuess }) {
   if (guess) {
     return (
       <div className="flex justify-center text-center">
-        <div className="flex flex-row">
-          {guess.map((letter, i) => {
+        <div className="flex flex-row row">
+          {guess.map((l, i) => {
             return (
               <div
-                className={`${letter.color} block items-center pt-1 w-[60px] h-[60px] border border-slate-500 m-2 text-center uppercase font-semibold text-5xl text-black`}
+                className={`${l.color} block items-center pt-1 w-[60px] h-[60px] border border-slate-500 m-2 text-center uppercase font-semibold text-5xl text-black`}
                 key={i}
               >
-                {letter.key}
+                {l.key}
               </div>
             );
           })}
         </div>
+      </div>
+    );
+  }
+  if (currentGuess) {
+    let letters = currentGuess.split("");
+
+    return (
+      <div className="flex justify-center text-center current">
+        {letters.map((letter, i) => (
+          <div
+            key={i}
+            className="block items-center pt-1 w-[60px] h-[60px] border border-slate-500 m-2 text-center uppercase font-semibold text-5xl text-black`"
+          >
+            {letter}
+          </div>
+        ))}
+        {/*
+            Needed for current guess to have empty squares
+            to map to
+        */}
+        {[...Array(5 - letters.length)].map((_, i) => (
+          <div
+            className="block w-[60px] h-[60px] border border-slate-500 m-2 text-center uppercase bold text-5xl"
+            key={i}
+          ></div>
+        ))}
       </div>
     );
   }
