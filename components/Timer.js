b950f19d556a5c939a26de954061a877e0d0688a
iglooe <day.derek7@protonmail.com>
2024-08-25 22:11:28 -0500

    oops

---
+++ components/Timer.js
@@ -0,0 +1,30 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+function Timer() {
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const formatTime = (totalSeconds) => {
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+
+    return [hours, minutes, seconds]
+      .map((v) => (v < 10 ? "0" + v : v))
+      .filter((v, i) => v !== "00" || i > 0)
+      .join(":");
+  };
+
+  return <div className="text-lg">{formatTime(seconds)}</div>;
+}
+
+export default Timer;
